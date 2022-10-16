c4d7000717b6ab5386a2f3dd62dce93da8de52b5
Upasana Chaudhuri <upasana chaudhuri0710@gmail.com>
2022-10-16 12:40:22 +0530

    main

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const url = require('url');
 
 const port = process.env.port || 3000;
 
-const server = http.createServer((req, res) =>{
+const server = http.createServer((req, res) => {
     const path = url.parse(req.url).pathname;
     const query = url.parse(req.url,true).query;
     if(path === '/add'){
         res.write(`${parseInt(query.no1)} + ${parseInt(query.no2)}\n`);
+        const sum = parseInt(query.no1) + parseInt(query.no2);
+        res.end(`Sum = ${sum}`);
     }
-    const sum = parseInt(query.no1) + parseInt(query.no2);
-    res.end(`Sum = ${sum}`);
 });
 
 server.listen(port, function(err){
